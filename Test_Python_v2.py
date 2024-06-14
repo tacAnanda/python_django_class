9540f2d47a59e6dc70c88e381b91f34cecbd6846
tacAnanda <tacAnanda@gmail.com>
2024-06-14 17:14:08 +0600

    First Commit

---
+++ Test_Python_v2.py
@@ -0,0 +1,25 @@
+print ("This is a Test")
+
+
+""" 
+This is a Multiline comment 
+Data in this block 
+willnot be visible 
+"""
+
+
+print ("End of Multiline Comment")
+
+myvar="Probal";
+print(myvar);
+    
+    
+    
+first_name=input("Enter First Name : ")
+last_name=input("Enter Last Name : ")
+
+user_Name=first_name + " " + last_name
+
+print(user_Name)
+
+
