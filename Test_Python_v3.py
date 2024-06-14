cd02d8b4cd4b252b4d212a99a0cee536011e5a0c
tacAnanda <tacAnanda@gmail.com>
2024-06-14 19:58:48 +0600

    first commit

---
+++ Test_Python_v3.py
@@ -0,0 +1,27 @@
+
+
+# x=["Dhaka", "Rajshahi", "Sylhet"]
+# print (x)
+# print (type(x))
+
+# y=("Mango", "Apple")
+# print (y)
+# print (type(y))
+
+# x1=input("Enter 1st Value ")
+# y1=input("Enter 2d Value ")
+# z1=input("Enter 3rd Value ")
+
+
+# if(x1>y1 and x1>z1):
+#     print ("X1 is MAX   " +x1)
+# elif(y1>x1 and y1>z1):
+#     print ("Y1 is MAX   "+y1)
+# elif(z1>x1 and z1>y1):
+#     print ("Z1 is MAX   "+z1)
+
+
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+print(thislist)
