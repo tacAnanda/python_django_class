8be88e5f121aa0238184c930dd502eb8bae318ea
tacAnanda <tacAnanda@gmail.com>
2024-06-22 23:58:45 +0600

    Project and App

---
+++ ECommerce_Solution/xGadget/xGadget_Main_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class XgadgetMainAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'xGadget_Main_App'
---
+++ eCommerce_Solution_2nd/yFasionHouse_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YfasionhouseAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yFasionHouse_App'
